da7efb978e259be4845866c35f27a82204fb2428
Jefferson Abreu <pereirajefferson06@gmail.com>
2021-03-29 11:09:45 -0300

    criado ambiente de desenvolvimento

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./master.css";
+
+console.log("Hello World");
---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+      main: "./src/index.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.html$/i,
+          use: ["html-loader"],
+        },
+        {
+          test: /\.(png|jpg|svg|gif)$/i,
+          use: {
+            loader: "file-loader",
+            options: {
+              publicPath: "assets",
+              name: "[name].[hash].[ext]",
+              outputPath: "assets",
+            },
+          },
+        },
+      ],
+    },
+  };
+  
