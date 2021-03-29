1eef2bf4efaac1a50a52c110cf7ed90a457c6f40
Jefferson Abreu <pereirajefferson06@gmail.com>
2021-03-29 11:23:08 -0300

    criado comando de build do projeto

---
+++ webpack.prod.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+});
