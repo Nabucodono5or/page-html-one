f3cd05703dbf26a5ec8c803f6bed7d5d6c1cbc47
Jefferson Abreu <pereirajefferson06@gmail.com>
2021-04-03 12:05:54 -0300

    inserido a estrutura imagem

--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,31 @@
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-      main: "./src/index.js",
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/template.html",
-      }),
+  entry: {
+    main: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      // {
+      //   test: /\.(png|jpg|svg|gif)$/i,
+      //   use: {
+      //     loader: "file-loader",
+      //     options: {
+      //       publicPath: "assets",
+      //       name: "[name].[hash].[ext]",
+      //       outputPath: "assets",
+      //     },
+      //   },
+      // },
     ],
-    module: {
-      rules: [
-        {
-          test: /\.html$/i,
-          use: ["html-loader"],
-        },
-        {
-          test: /\.(png|jpg|svg|gif)$/i,
-          use: {
-            loader: "file-loader",
-            options: {
-              publicPath: "assets",
-              name: "[name].[hash].[ext]",
-              outputPath: "assets",
-            },
-          },
-        },
-      ],
-    },
-  };
-  
+  },
+};
