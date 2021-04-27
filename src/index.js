e0912546651e66fa25985450e9c0943effc0448a
Jefferson Abreu <pereirajefferson06@gmail.com>
2021-04-27 16:12:15 -0300

    criado efeito slide com javascript

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
 import "./master.css";
 import "../node_modules/@fortawesome/fontawesome-free/js/all.min";
 
-console.log("Hello World");
+let dotOne = document.getElementById("one");
+let dotTwo = document.getElementById("two");
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("myslides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+dotOne.addEventListener("click", () => showSlides(slideIndex = 1));
+dotTwo.addEventListener("click", () => showSlides(slideIndex = 2));
